7a6cae9e9cda6ba34056dce2df43984dee647db7
debashis <tripathydebashis124@gmail.com>
2025-09-14 11:35:44 +0530

    Add

--- Assignment1/q12.java
+++ Assignment1/q12.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 class q12 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
