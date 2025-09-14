81e9174acbaf221b473ba625fc8aa863c028107b
debashis <tripathydebashis124@gmail.com>
2025-09-14 11:32:32 +0530

    Add

---
+++ Assignment1/q1.java
@@ -0,0 +1,9 @@
+
+class q1 {
+    public static void main(String[] args) {
+        System.out.println("Name: Debashis Tripathy");
+        System.out.println("Branch: CSE");
+        System.out.println("Roll No: 06");
+        System.out.println("College: Silicon University");
+    }
+}
